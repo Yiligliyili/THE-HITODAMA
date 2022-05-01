f27a5afb7813bb9d82e7d3bd02f4a9bfd6f7edc6
PirisAran <apiris@edu.tecnocampus.cat>
2022-05-01 19:23:22 +0200

    a ver

---
+++ THE HITODAMA Unity/Assets/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    public Vector2 maxPose;
+    public Vector2 minPose;
+
+    private void FixedUpdate()
+    {
+        if (transform.position !=  target.position)
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y,transform.position.z);
+            
+            targetPos.x = Mathf.Clamp(targetPos.y, maxPose.y, minPose.y);
+            targetPos.x = Mathf.Clamp(targetPos.x, maxPose.x, minPose.x);
+
+            transform.position = Vector3.Lerp(targetPos, target.position, smoothing);
+
+
+
+        }
+
+    }
+}
